581de2584d895f91834533e0bb7a8bdf647ea50c
Kev On Laptop <kgroves@houseofkiya.co.uk>
2024-03-02 17:03:29 +0000

    lang update

--- os/forth_words.asm
+++ os/forth_words.asm
@@ -69,7 +69,7 @@ sysdict:
 .PLUS:	db 2     
 	dw .NEG
         db 2
-	db "+",0          ; + ( u u -- u )    Add two numbers and push result
+	db "+",0          ; | + ( u u -- u )    Add two numbers and push result   |DONE
 		; add top two values and push back result
 
 
@@ -112,33 +112,33 @@ sysdict:
 .NEG:	db 3
 	dw .DIV
         db 2
-	db "-",0    ; - ( u1 u2 -- u )    Subtract u2 from u1 and push result
+	db "-",0    ; | - ( u1 u2 -- u )    Subtract u2 from u1 and push result  |
 		NEXT
 .DIV:	db 4
 	dw .MUL
 	db 2
-	db "/",0     ; / ( u1 u2 -- u )     Divide u1 by u2 and push result
+	db "/",0     ; | / ( u1 u2 -- u )     Divide u1 by u2 and push result |
 		NEXT
 .MUL: 	db 5
 	dw .DUP
 	db 2
-	db "*",0     ; * ( u1 u2 -- u )     Multiply TOS and push result
+	db "*",0     ; | * ( u1 u2 -- u )     Multiply TOS and push result |
 		NEXT
 .DUP:	db 6
 	dw .EMIT
 	db 4
-	db "DUP",0   ; DUP ( u -- u u )     Duplicate whatever item is on TOS
+	db "DUP",0   ; | DUP ( u -- u u )     Duplicate whatever item is on TOS |
 		NEXT
 .EMIT:	db 7
 	dw .DOT
 	db 5
-	db "EMIT",0  ;  EMIT ( u -- )        Display TOS
+	db "EMIT",0  ;|  EMIT ( u -- )        Display TOS   |DONE
 		jp .print
 		NEXT
 .DOT:	db 8
 	dw .SWAP
 	db 2
-	db ".",0         ; . ( u -- )    Display TOS 
+	db ".",0         ;| . ( u -- )    Display TOS   |DONE
 		; get value off TOS and display it
 
 		.print:
@@ -180,139 +180,193 @@ sysdict:
 .SWAP:	db 9
 	dw .IF
 	db 5
-	db "SWAP",0    ; SWAP ( w1 w2 -- w2 w1 )    Swap top two items (of whatever type) on TOS
+	db "SWAP",0    ; |SWAP ( w1 w2 -- w2 w1 )    Swap top two items (of whatever type) on TOS
+;		FORTH_DSP
+;		ex de, hl
+;		ld hl,(de)
+;
+;		push hl
+;		FORTH_DSP
+;		dec hl
+;		dec hl
+
 		NEXT
 .IF:	db 10
 	dw .THEN
 	db 3
-	db "IF",0     ;  IF ( w -- f )     If TOS is true exec code following before??
+	db "IF",0     ;  |IF ( w -- f )     If TOS is true exec code following before??
 		NEXT
 .THEN:	db 11
 	dw .ELSE
 	db 5
-	db "THEN",0    ; THEN ( -- )     control????
+	db "THEN",0    ; |THEN ( -- )     control????
 		NEXT
 .ELSE: 	db 12
 	dw .DO
 	db 5
-	db "ELSE",0      ; ELSE ( -- )     control???
+	db "ELSE",0      ; |ELSE ( -- )     control???
 		NEXT
 .DO:	db 13
 	dw .LOOP
 	db 3
-	db "DO",0       ; DO ( u -- )        TOS has loop count until LOOP
+	db "DO",0       ; |DO ( u -- )        TOS has loop count until LOOP
 		NEXT
 .LOOP:	db 14
 	dw .COLN
 	db 5
-	db "LOOP",0      ; LOOP ( -- )     Current loop end marker
+	db "LOOP",0      ; |LOOP ( -- )     Current loop end marker
 		NEXT
 .COLN:	db 15
 	dw .SCOLN
 	db 2
-	db ":",0     ; : ( -- )         Create new word
+	db ":",0     ; |: ( -- )         Create new word
 		NEXT
 .SCOLN:	db 16
 	dw .DROP
 	db 2
-	db ";",0          ; ; ( -- )     Terminate new word
+	db ";",0          ; |; ( -- )     Terminate new word
+
+
+
 		NEXT
 .DROP:   db 17
 	dw .DUP2
 	db 5
-	db "DROP",0        ; DROP ( w -- )   drop the TOS item
+	db "DROP",0        ; |DROP ( w -- )   drop the TOS item   |DONE
+		FORTH_DSP_POP
 		NEXT
 .DUP2:	db 18
 	dw .DROP2
 	db 5
-	db "2DUP",0      ; 2DUP ( w1 w2 -- w1 w2 w1 w2 ) Duplicate the top two items on TOS  
+	db "2DUP",0      ; |2DUP ( w1 w2 -- w1 w2 w1 w2 ) Duplicate the top two items on TOS  
 		NEXT
 .DROP2:	db 19
 	dw .SWAP2
 	db 6
-	db "2DROP",0      ; 2DROP ( w w -- )    Double drop
+	db "2DROP",0      ; |2DROP ( w w -- )    Double drop
 		NEXT
 .SWAP2:	db 20
 	dw .AT
 	db 5
-	db "2SWAP",0      ; 2SWAP ( w1 w2 w3 w4 -- w3 w4 w1 w2 ) Swap top pair of items
+	db "2SWAP",0      ; |2SWAP ( w1 w2 w3 w4 -- w3 w4 w1 w2 ) Swap top pair of items
 		NEXT
 .AT:	db 21
 	dw .CAT
 	db 2
-	db "@",0         ; @ ( w -- ) Push onto TOS byte stored at address
+	db "@",0         ;| @ ( w -- ) Push onto TOS byte stored at address   | DONE
+
+.getbyteat:	
+		FORTH_DSP_VALUEHL     			; TODO skip type check and assume number.... lol
+		
+		push hl
+	
+		FORTH_DSP_POP  ; TODO add stock underflow checks and throws 
+
+		pop hl
+
+		ld a, (hl)
+
+		ld l, a
+		ld h, 0
+		call forth_push_numhl
+
 		NEXT           
 .CAT:	db 22
 	dw .BANG
 	db 3
-	db "C@",0        ; C@  ( w -- ) Push onto TOS byte stored at address
+	db "C@",0        ; |C@  ( w -- ) Push onto TOS byte stored at address   |DONE
+		jp .getbyteat
 		NEXT
 .BANG:   db 23
 	dw .CBANG
 	db 2
-	db "!",0        ; ! ( x w -- ) Store x at address w
+	db "!",0        ; |! ( x w -- ) Store x at address w      | DONE
+
+.storebyteat:		
+		FORTH_DSP_VALUEHL     			; TODO skip type check and assume number.... lol
+		
+		push hl
+	
+		FORTH_DSP_POP  ; TODO add stock underflow checks and throws 
+
+		; get byte to poke
+
+		FORTH_DSP_VALUEHL     			; TODO skip type check and assume number.... lol
+		push hl
+
+
+		FORTH_DSP_POP  ; TODO add stock underflow checks and throws 
+
+
+		pop de
+		pop hl
+
+		ld (hl),e
+
+
 		NEXT
 .CBANG:	db 24
 	dw .LZERO
 	db 3
-	db "C!",0       ; C!  ( x w -- ) Store x at address w
+	db "C!",0       ; |C!  ( x w -- ) Store x at address w  | DONE
+		jp .storebyteat
 		NEXT
 .LZERO:	db 25
 	dw .TZERO
 	db 3
-	db "0<",0       ; 0< ( u -- f ) Push true if u is less than o
+	db "0<",0       ; |0< ( u -- f ) Push true if u is less than o
 		NEXT
 .TZERO:  db 26
 	dw .LESS
 	db 3
-	db "0=",0         ; 0= ( u -- f ) Push true if u equals 0
+	db "0=",0         ; |0= ( u -- f ) Push true if u equals 0
 		NEXT
 .LESS:   db 27
 	dw .GT
 	db 2
-	db "<",0         ; < ( u1 u2 -- f ) True if u1 is less than u2 
+	db "<",0         ; |< ( u1 u2 -- f ) True if u1 is less than u2 
 		NEXT
 .GT:	db 28
 	dw .EQUAL
 	db 2
-	db ">",0       ; > ( u1 u2 -- f ) True if u1 is greater than u2
+	db ">",0       ; |> ( u1 u2 -- f ) True if u1 is greater than u2
 		NEXT
 .EQUAL:  db 29
 	dw .SCALL
 	db 2
-	db "=",0          ; = ( u1 u2 -- f ) True if u1 equals u2
+	db "=",0          ; |= ( u1 u2 -- f ) True if u1 equals u2
 		NEXT
 .SCALL:	db 30
 	dw .SIN
 	db 5
-	db "CALL",0	; CALL ( w -- ) machine code call to address w  
+	db "CALL",0	; |CALL ( w -- ) machine code call to address w  
 		NEXT
 .SIN:	db 31
 	dw .SOUT
 	db 3
-	db "IN",0       ; IN ( u1-- u )    Perform z80 IN with u1 being the port number. Push result to TOS
+	db "IN",0       ; |IN ( u1-- u )    Perform z80 IN with u1 being the port number. Push result to TOS
 		NEXT
 .SOUT:   db 32
 	dw .CLS
 	db 4
-	db "OUT",0      ; OUT ( u1 u2 -- ) Perform Z80 OUT to port u2 sending byte u1
+	db "OUT",0      ;| OUT ( u1 u2 -- ) Perform Z80 OUT to port u2 sending byte u1
 		NEXT
 
 .CLS:   db 33
 	dw .DRAW
 	db 4
-	db "CLS",0     ; CLS ( -- ) clear frame buffer
+	db "CLS",0     ; |CLS ( -- ) clear frame buffer    |DONE
 		call clear_display
 		NEXT
 
 .DRAW:   db 34
 	dw .DUMP
 	db 5
-	db "DRAW",0     ; DRAW ( -- ) Draw contents of current frame buffer
+	db "DRAW",0     ; |DRAW ( -- ) Draw contents of current frame buffer  | DONE
 		call update_display
 		NEXT
 
-.DUMP:   db 35				; DUMP ( x --  ) With address x display dump
+.DUMP:   db 35				; |DUMP ( x --  ) With address x display dump   |DONE
 	dw .CDUMP
 	db 5
 	db "DUMP",0
@@ -337,7 +391,7 @@ sysdict:
 .CDUMP:   db 36                      ; continue memory dump
 	dw .DEPTH
 	db 6
-	db "CDUMP",0              ; CDUMP ( -- ) continue dump of memory from DUMP
+	db "CDUMP",0              ; |CDUMP ( -- ) continue dump of memory from DUMP |  DONE
 		call clear_display
 		call dumpcont	
 		ret			; TODO command causes end of remaining parsing so cant do: $0000 DUMP CDUMP $8000 DUMP
@@ -347,43 +401,43 @@ sysdict:
 .DEPTH:   db 37                     ; stack count
 	dw .DIR
 	db 6
-	db "DEPTH",0             ; DEPTH ( -- u ) Push count of stack
+	db "DEPTH",0             ; |DEPTH ( -- u ) Push count of stack
 		NEXT
 
 .DIR:   db 38                     ;
 	dw .SAVE
 	db 4
-	db "DIR",0               ; DIR ( u -- w... u )   Using bank number u push directory entries w with count u 
+	db "DIR",0               ; |DIR ( u -- w... u )   Using bank number u push directory entries w with count u 
 		NEXT
 .SAVE:   db 39
 	dw .LOAD
 	db 5
-	db "SAVE",0              ; SAVE  ( w u -- )    Save user word memory to file name w on bank u
+	db "SAVE",0              ; |SAVE  ( w u -- )    Save user word memory to file name w on bank u
 		NEXT
 .LOAD:   db 40
 	dw .DISPLAY
 	db 5
-	db "LOAD",0               ; LOAD ( w u -- )    Load user word memory from file name w on bank u
+	db "LOAD",0               ;| LOAD ( w u -- )    Load user word memory from file name w on bank u
 		NEXT
 .DISPLAY:   db 41                     
 	dw .KEY
 	db 8
-	db "DISPLAY",0            ; DISPLAY ( w u1 u2 -- )  Write to current frame buffer at row u2 col u1 the var of any type w
+	db "DISPLAY",0            ;| DISPLAY ( w u1 u2 -- )  Write to current frame buffer at row u2 col u1 the var of any type w
 		NEXT
 .KEY:   db 42               
 	dw .WAITK
 	db 4
-	db "KEY",0     ; KEY ( -- w f )      scan for keypress but do not wait true if next item on stack is key press
+	db "KEY",0     ; |KEY ( -- w f )      scan for keypress but do not wait true if next item on stack is key press
 		NEXT
 .WAITK:   db 43               
 	dw .ACCEPT
 	db 6
-	db "WAITK",0     ; KEY ( -- w )      wait for keypress TOS is key press
+	db "WAITK",0     ;| WAITK ( -- w )      wait for keypress TOS is key press
 		NEXT
 .ACCEPT:   db 44               
 	dw .V0
 	db 7
-	db "ACCEPT",0     ; KEY ( -- w )    Prompt for text input and push pointer to string
+	db "ACCEPT",0     ; |ACCEPT ( -- w )    Prompt for text input and push pointer to string
 		NEXT
 
 ;;;; counter gap
@@ -454,7 +508,7 @@ sysdict:
 .I:   db 153               
 	dw .END
 	db 2
-	db "I",0               ; ( -- ) Loop counter
+	db "I",0               ;| I ( -- ) Loop counter
 		NEXT
 
 
---
+++ os/wordlist.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "Words List"
+echo "------------------"
+grep "|" <forth_words.asm   | cut -f2- -d'|'
+
+echo "Words ready to use"
+echo "------------------"
+grep "|" <forth_words.asm   | cut -f2- -d'|' | grep DONE
+
+echo "Words still left to do"
+echo "----------------------"
+grep "|" <forth_words.asm   | cut -f2- -d'|' | grep DONE -v
+

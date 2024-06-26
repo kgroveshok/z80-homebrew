56a4587433685570a1324c7422c54c2aeb2f4210
Kev On Laptop <kgroves@houseofkiya.co.uk>
2024-06-26 08:16:50 +0100

    make word list nicer

--- os/wordlist.sh
+++ os/wordlist.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-echo "Forth Language Reference"
-echo "------------------------"
-cat forth_words_*.asm | grep "|"  | cut -f2- -d'|' 
+echo "# Forth Language Reference"
+echo
+cat forth_words_*.asm | grep "|"  | cut -f2- -d'|'| sed 's/^ /* /g' | sed 's/^* |//g' 
 
-echo "Words Ready To Use"
-echo "------------------"
-cat forth_words_*.asm | grep -v "; | | " | grep "|" | cut -f2- -d'|' | grep DONE
+echo "# Words Ready To Use"
+echo
+cat forth_words_*.asm | grep -v "; | | " | grep "|" | cut -f2- -d'|' | grep DONE | sed 's/^ /* /g' | sed 's/^* |//g' 
 
-echo "Words Still Left To Do"
-echo "----------------------"
-cat forth_words_*.asm | grep -v "; | | " | grep "|" | cut -f2- -d'|' | grep DONE -v
+echo "# Words Still Left To Do"
+echo
+cat forth_words_*.asm | grep -v "; | | " | grep "|" | cut -f2- -d'|' | grep DONE -v | sed 's/^ /* /g' | sed 's/^* |//g' 
 

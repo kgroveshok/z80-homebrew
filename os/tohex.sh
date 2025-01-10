946aa6e93fcbe31a7014ed4f362de38c9540e090
Kev On Laptop <kgroves@houseofkiya.co.uk>
2025-01-10 19:21:15 +0000

    added bin to git

--- os/tohex.sh
+++ os/tohex.sh
@@ -5,6 +5,8 @@ z80asm  -l $1.asm   --label=$1.sym
 if [[ $? -eq 0 ]] ; then
 ./bin2hex.py -b a.bin -o $1.hex -A $2
 fi
+cp a.bin $1.bin
+
 
 #objcopy --input-target=binary --output-target=ihex a.bin $1.hex
 #cat $1.asm

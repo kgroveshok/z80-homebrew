52a945121a9bbadfefef0d087e2037d07b083b91
Kev On Laptop <kgroves@houseofkiya.co.uk>
2025-02-03 20:42:45 +0000

    Get a proper listing

--- os/tohex.sh
+++ os/tohex.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-z80asm  -l $1.asm   --label=$1.sym
+rm -fv a.bin
+
+z80asm  -l $1.asm   --label=$1.sym 2> >(tee $1.lst)
 
 if [[ $? -eq 0 ]] ; then
 ./bin2hex.py -b a.bin -o $1.hex -A $2

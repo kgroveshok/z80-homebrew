7f6f2f9cba2d60b9215a46ff44b3d6bccf3a270b
Kev On Laptop <kgroves@houseofkiya.co.uk>
2025-07-29 19:39:46 +0100

    Stop building mini format as just too restricted

--- os/make.sh
+++ os/make.sh
@@ -2,10 +2,10 @@
 
 # Make OS versions for various hardware configs
 
-./tohex.sh os_mini 0
+#./tohex.sh os_mini 0
 ./tohex.sh os_mega 0
 
-./tohex.sh os_mini_sc114 8000
+#./tohex.sh os_mini_sc114 8000
 ./tohex.sh os_mega_sc114 8000
 
 # TODO fix up CPM
